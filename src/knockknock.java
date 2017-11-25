cd3b5a700363e185d2bd1dfdcae4bdcf311de5a2
DavidAndBlake <League@administorsimac.attlocal.net>
2017-11-25 11:25:04 -0800

    knockknoch,robot griffity, Last summer IP

---
+++ src/knockknock.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class knockknock {
+public static void main(String[] args) {
+	String joke =JOptionPane.showInputDialog(null,"Knock Knock" );
+	JOptionPane.showMessageDialog(null, "broken pencil");
+	JOptionPane.showMessageDialog(null, "oh, never mind, its pointless");
+	
+}}
---
+++ src/robotgriffity.java
@@ -0,0 +1,28 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class robotgriffity {
+public static void main(String[] args) {
+	Robot john = new Robot();
+	john.penDown();
+	john.setSpeed(10);
+	john.move(300);
+   john.turn(180);
+   john.move(100);
+   john.setAngle(45);
+   john.move(200);
+    john.turn(180);
+    john.move(200);
+    john.turn(45);
+    john.turn(90);
+     john.turn(135);
+     john.move(250);
+     john.turn(360);
+     
+}
+
+
+
+
+
+
+}
