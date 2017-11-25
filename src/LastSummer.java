8d6b458dfd2ac622018b15db10e19cf418b35a10
DavidAndBlake <League@administorsimac.attlocal.net>
2017-11-25 11:25:42 -0800

    last summer

--- src/LastSummer.java
+++ src/LastSummer.java
@@ -1,4 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class LastSummer {
+public static void main(String[] args) {
+	String thing=JOptionPane.showInputDialog("What is your name");
+	JOptionPane.showMessageDialog()		
+
+
+
+}
 
 }
